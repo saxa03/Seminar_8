8192ecf30e4f10d9105bc46501061103ea3ee61a
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-15 19:52:24 +0800

    add Task 053

---
+++ Task053/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задача 53: Задайте двумерный массив. 
+//Напишите программу, которая поменяет местами первую и последнюю строку массива.
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+WriteLine();
+ChangeRows(array);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+int[,] result = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+result[i, j] = new Random().Next(min, max + 1);
+}
+
+}
+return result;
+}
+void PrintArray(int[,] inArray)
+{
+for (int i = 0; i < inArray.GetLength(0); i++)
+{
+for (int j = 0; j < inArray.GetLength(1); j++)
+{
+Write($"{inArray[i, j]} ");
+}
+WriteLine();
+}
+}
+
+void ChangeRows(int[,] inArray)
+{
+int rowCount = inArray.GetLength(0) - 1;
+for (int i = 0; i < inArray.GetLength(1); i++)
+{
+int k = inArray[0, i];
+inArray[0, i] = inArray[rowCount, i];
+inArray[rowCount, i] = k;
+}
+}
