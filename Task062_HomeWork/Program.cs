3c9627e210dbaf2479f3e03c1aa4a83cf3f3efd6
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-17 12:20:00 +0800

    add Task062 HomeWork

---
+++ Task062_HomeWork/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+//  Например, на выходе получается вот такой массив:
+//  01 02 03 04
+//  12 13 14 05
+//  11 16 15 06
+//  10 09 08 07
+using System;
+using static System.Console;
+
+Clear();
+int size = 4;
+int[,] matrix = new int[size, size];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+  matrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+PrintArray(matrix);
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Write($"{matr[i, j]} ");
+        }
+        WriteLine();
+    }
+}
